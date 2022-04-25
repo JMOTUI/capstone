8e9077c5e042d39b20095e217157f036b44d8535
Jens Mohme <Jens.mohme@tui.com>
2022-04-25 13:50:50 +0000

    edited app to prove migration

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World!!"
+    return "Hello World!! This is the modified app after migration!!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
